8377013f72b75a0cabb6a87b6f94533d4566fd6d
Julian Kornberger <jk@digineo.de>
2021-12-30 19:33:31 +0100

    UltraMaXX MK reader

--- airsensor.ino
+++ airsensor.ino
@@ -1,30 +1,38 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <PubSubClient.h>
+#include "EspMQTTClient.h"
 #include "settings.h"
 
 #define baudrate 115200
-WiFiClient wifiClient;
-
-#ifdef MQTT
-PubSubClient client(wifiClient);
-#endif
-
-struct {
-  bool read;
-  bool valid;
-  int ppm;
-} zh18_result = { .read = true };
-
-char hostString[20] = {0};
+#define MAX_DATA_SIZE 128
+#define LED_BUILTIN 2
+#define RXD2 9
+#define TXD2 10
 
 // All timestamps/periods in milliseconds
 unsigned long uptime = 0;
 unsigned long last_send = 0;
-const unsigned long sending_interval = 15 * 60 * 1000;
+const unsigned long sending_interval = 600 * 1000;
+
+#define INIT_CHAR 0x55
+#define INIT_BAUD 2400
+#define INIT_SIZE 504
+
+EspMQTTClient client(
+  wifi_ssid,
+  wifi_key,
+  mqtt_server,  // MQTT Broker server ip
+  "",           // Can be omitted if not needed
+  "",           // Can be omitted if not needed
+  "WMZ"         // Client name that uniquely identify your device
+);
 
 
-void debugf(char *fmt, ... ) {
+struct {
+  char data[MAX_DATA_SIZE];
+  int length;
+} response = { .length = 0 };
+
+
+void debugf(const char *fmt, ... ) {
   char buf[128]; // resulting string limited to 128 chars
   va_list args;
   va_start (args, fmt );
@@ -39,196 +47,97 @@ void debugf_float(char *fmt, float val) {
   debugf(fmt, buf);
 }
 
-String Float2String(const float value) {
-  // Convert a float to String with two decimals.
-  char temp[12];
-  String s;
 
-  dtostrf(value, 8, 2, temp);
-  s = String(temp);
-  s.trim();
-  return s;
-}
-
-#define INIT_CHAR 0x55
-#define INIT_BAUD 2400
-#define INIT_SIZE 522 // 504 steht in Doku?
-
-void readWmz()
+bool readWmz()
 {
-  Serial.println("read Wmz");
+  debugf("reading WMZ\n");
+  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on
   Serial.flush();
-  Serial.swap();
-  Serial.begin(INIT_BAUD, SERIAL_8N1);
 
   // wakeup sequence
   byte wakeup[8];
   memset(wakeup, INIT_CHAR, sizeof(wakeup));
 
+  unsigned long started = millis(), finished = 0;
+
+  Serial2.begin(INIT_BAUD, SERIAL_8N1);
+  
   // write sequence
-  for (int i = 0; i < INIT_SIZE; ) {
-    i += Serial.write(wakeup, sizeof(wakeup));
+  int written;
+  for (written = 0; written < INIT_SIZE; ) {
+    written += Serial2.write(wakeup, sizeof(wakeup));
   }
+  Serial2.flush();
+
+  finished = millis();
 
   // sleep
-  delay(110);
+  delay(120);
 
-  Serial.begin(INIT_BAUD, SERIAL_8E1);
+  Serial2.begin(INIT_BAUD, SERIAL_8E1);
 
   // Sende Anfrage Daten Klasse 2
   char cmd[] = { 0x10, 0x5B, 0x00, 0x5B, 0x16 };
-  Serial.write(cmd, sizeof(cmd));
+  Serial2.write(cmd, sizeof(cmd));
+  Serial2.flush();
+
+  debugf("sent wakeup sequence: %d bytes at %d baud in %d ms\n", written, INIT_BAUD, finished-started);
 
   // Antwort empfangen
-  char response[128];
-  int read = Serial.readBytes(response, sizeof(response));
+  char *data = response.data;
+  response.length = Serial2.readBytes(data + response.length, MAX_DATA_SIZE);
+  debugf("received %d bytes\n", response.length);
 
-  // back to default
-  Serial.swap();
-  Serial.begin(baudrate);
-
-  if (!read) {
-    Serial.println("no bytes received");
-  } else {
-    Serial.print("received ");
-    Serial.print(read);
-    Serial.println(" bytes");
+  // Turn the LED off
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  if (!response.length) {
+    return false;
   }
-  for (int i = 0; i < read; i++) {
-    Serial.print(response[i], HEX);
+  
+  for (int i = 0; i < response.length; i++) {
+    debugf("%02x", data[i]);
   }
+  debugf("\n");
 
-  if (response[0] != 0x68)
+  if (data[0] != 0x68)
   {
-    Serial.println("[zh18] Wrong starting byte received");
-    return;
+    debugf("Wrong starting byte received");
+    return false;
   }
-}
-
-char* Statuses[] = {
-  "Idle",
-  "SSID not available",
-  "Scan completed",
-  "Connected",
-  "Connect failed",
-  "Connection lost",
-  "Disconnected"
-};
-
-
-void connectWifi() {
-  Serial.printf("Connecting to %s ", wifi_ssid);
-
-  WiFi.begin(wifi_ssid, wifi_pass);
-
-  while (true) {
-    Serial.print(".");
-    int status;
-    int new_status = WiFi.status();
-
-    if (new_status != status) {
-      status = new_status;
-
-      Serial.printf("\nWifi status: %s\n", Statuses[status]);
-    }
-
-    if (status == WL_CONNECTED)
-      break;
-
-    if (status == WL_NO_SSID_AVAIL) {
-      delay(3000);
-      ESP.restart();
-    }
-
-    delay(100);
-  }
-  Serial.println(" done");
-}
-
-void initWifi() {
-  int status = -1;
-
-  WiFi.hostname(hostString);
-  WiFi.softAPdisconnect(true);
-  ESP.wdtEnable(1000);
-
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Wifi already connected");
-  } else {
-    connectWifi();
-  }
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  
+  return true;
 }
 
 void sendData() {
-  String data, url;
-
-  // Build data
-  data = "";
-  data += F("sensors,host=");
-  data += hostString;
-  data += F(",location=");
-  data += location;
-  data += " ";
-
-  if (zh18_result.valid) {
-    data += F("co2=");
-    data += Float2String(zh18_result.ppm);
-    data += F(",");
-  }
-
-  if (!data.endsWith(",")) {
-    debugf("No data available\n");
-    return;
-  }
-
-  data += F("uptime=");
-  data += String(uptime / 1000);
-
-  // Build URL
-  url = String(influx_url);
-  url += F("write?precision=s&db=");
-  url += influx_database;
-
-#ifdef MQTT
   // Send data to MQTT
-  client.publish(mqtt_topic, String(data).c_str(), true);
-#endif
+  client.publish(mqtt_topic, (const uint8_t*) response.data, (unsigned int) response.length, true);
+}
 
-  // Send data to InfluxDB
-  HTTPClient http;
-  http.begin(wifiClient, url);
-  http.setAuthorization(influx_user, influx_pass);
-  http.addHeader("Content-Type", "text/plain");
-  int status = http.POST(data);
-  if (status != 204) {
-    Serial.printf("[http] unexpected status code: %d\n", status);
-    http.writeToStream(&Serial);
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED pin as an output
+  Serial.begin(baudrate);
+
+  uint32_t chipId = 0;
+  for(int i=0; i<17; i=i+8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
   }
-  http.end();
+  Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
+  Serial.printf("This chip has %d cores\n", ESP.getChipCores());
+  Serial.print("Chip ID: ");
+  Serial.println(chipId);
 
+  client.enableDebuggingMessages();
 }
 
 
-void setup() {
-  Serial.begin(baudrate);
-
-  sprintf(hostString, "esp8266-%06X", ESP.getChipId());
-  Serial.printf("\nHostname: %s\n", hostString);
-
-  // Serial.setDebugOutput(true);
-  initWifi();
-
-#ifdef MQTT
-  client.setServer(mqtt_server, 1883);
-#endif
-
-  delay(1000);
+void onConnectionEstablished() {
+  Serial.println("connection established");
 }
 
 
 void loop() {
+  client.loop();
   uptime = millis();
 
   // uptime restarted at zero? (overflow after 50 days)
@@ -236,18 +145,18 @@ void loop() {
     last_send = 0;
 
   // Sending now?
-  if (last_send == 0 || last_send + sending_interval < uptime) {
-    debugf("\nreading sensors ...\n");
-    debugf("Uptime:        %04d\n", uptime / 1000);
+  if (client.isConnected() && (last_send == 0 || last_send + sending_interval < uptime)) {
+    debugf("Uptime: %04d\n", uptime / 1000);
 
-    readWmz();
+    if (readWmz()){
+      last_send = uptime;
+      sendData();
+      debugf("finished successfully\n");
+    }else{
+      debugf("failed\n");
+      delay(1000);
+    }
 
-    if (WiFi.status() != WL_CONNECTED)
-      initWifi();
-
-    sendData();
-
-    last_send = uptime;
-    debugf("done\n");
+    Serial.flush();
   }
 }
